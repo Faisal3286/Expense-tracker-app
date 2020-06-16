3b530f72f0c8fec376c848ee0537368210d83717
Faisal nabeel <56247620+Faisal3286@users.noreply.github.com>
2020-06-16 13:08:38 +0500

    Adding staying

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = () => {
     return (
-        <div className='dib br1 pa3 grow  shadow-5'>
+        <div className='dib br1 pa2 grow  shadow-5'>
             <h2>
                 Expense Tracker 
             </h2>
